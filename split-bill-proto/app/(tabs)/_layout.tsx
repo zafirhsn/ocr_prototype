ed1a7e16c18aa343f77a1caffd37569b4a82b0cb
Zafir Hasan <zafirhsn@gmail.com>
2025-01-02 00:03:25 -0500

    fix: tab nav working, adding redux package

--- split-bill-proto/app/(tabs)/_layout.tsx
+++ split-bill-proto/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from 'expo-router';
+import { Tabs, useRouter, Slot } from 'expo-router';
 import React from 'react';
 import { Platform, StyleSheet } from 'react-native';
 
@@ -11,19 +11,36 @@ import { Layout, BottomNavigation, BottomNavigationTab } from '@ui-kitten/compon
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const router = useRouter();
   const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const navigateTo = (index) => {
+    setSelectedIndex(index);
+    if (index === 0) router.push('/(tabs)/add-bill');
+    if (index === 1) router.push('/(tabs)/add-diners');
+    if (index === 2) router.push('/(tabs)/hydrate-diners');
+    if (index === 3) router.push('/(tabs)/summary');
+  };
 
   return (
     // TODO: Fix bottom navigation taking up whole screen
     <Layout
       style={styles.container}>
+      
+      <Layout
+        style={styles.childContainer}>
+        <Slot />
+      </Layout>
+
+      {/* TODO: Add route guards */} 
       <BottomNavigation
         selectedIndex={selectedIndex}
-        onSelect={index => setSelectedIndex(index)}
+        onSelect={navigateTo}
       >
-        <BottomNavigationTab title='USERS' />
-        <BottomNavigationTab title='ORDERS' />
-        <BottomNavigationTab title='TRANSACTIONS' />
+        <BottomNavigationTab title='Bill' />
+        <BottomNavigationTab title='Diners' />
+        <BottomNavigationTab title='Items' />
+        <BottomNavigationTab title='Summary' />
       </BottomNavigation>
     </Layout>
 
@@ -32,11 +49,15 @@ export default function TabLayout() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    position: 'absolute',
+    flex: 1,
+    // position: 'absolute',
     bottom: 0,
     width: '100%',
     // height: '10%',
     padding: '0%'
   },
+  childContainer: {
+    flex: 10,
+    width: '100%',
+  }
 });
